299a0e0b99e768f6c7392caeee9db7272ab00fb9
James Hunter <james.hunter@starlingbank.com>
2023-12-03 19:40:51 +0000

    Day 2b

--- days/day02/day2.test.ts
+++ days/day02/day2.test.ts
@@ -1,6 +1,6 @@
 import { expect, test, describe } from 'bun:test';
 import dedent from 'dedent';
-import { solvePart1 } from './day2';
+import { solvePart1, solvePart2 } from './day2';
 import day2Input from './day2.input';
 
 describe('Day 2', () => {
@@ -19,6 +19,24 @@ describe('Day 2', () => {
     test('real puzzle', () => {
       const result = solvePart1(day2Input);
       expect(result).toBe(2879);
+    });
+  });
+
+  describe('part 2', () => {
+    test('test case', () => {
+      const input = dedent`
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+      `;
+      expect(solvePart2(input)).toBe(2286);
+    });
+
+    test('real puzzle', () => {
+      const result = solvePart2(day2Input);
+      expect(result).toBe(65122);
     });
   });
 });
--- days/day02/day2.ts
+++ days/day02/day2.ts
@@ -27,3 +27,23 @@ export function solvePart1 (input: string): number {
     .map(processLine)
     .reduce((total, value) => total + value);
 }
+
+const processLineAgain = (line: string): number => {
+  const reds = line.match(/\d+(?= red)/g);
+  const greens = line.match(/\d+(?= green)/g);
+  const blues = line.match(/\d+(?= blue)/g);
+
+  if (!reds || !greens || !blues) return 0;
+
+  const largestRed = Math.max(...reds.map(red => Number(red)));
+  const largestGreen = Math.max(...greens.map(green => Number(green)));
+  const largestBlue = Math.max(...blues.map(blue => Number(blue)));
+
+  return largestRed * largestGreen * largestBlue;
+}
+
+export function solvePart2 (input: string): number {
+  return input.split('\n')
+    .map(processLineAgain)
+    .reduce((total, value) => total + value);
+}
