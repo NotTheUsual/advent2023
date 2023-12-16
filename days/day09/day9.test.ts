ccb37d8cfa91006349b5abe764b3e932eacdbaa4
James Hunter <james.hunter@starlingbank.com>
2023-12-16 15:20:17 +0000

    Day 9b

--- days/day09/day9.test.ts
+++ days/day09/day9.test.ts
@@ -1,6 +1,6 @@
 import { expect, test, describe } from 'bun:test';
 import dedent from 'dedent';
-import { solvePart1 } from './day9';
+import { solvePart1, solvePart2 } from './day9';
 import day9Input from './day9.input';
 
 describe('Day 9', () => {
@@ -16,6 +16,22 @@ describe('Day 9', () => {
 
     test('real puzzle', () => {
       const result = solvePart1(day9Input);
+      expect(result).toBe(2954600);
+    });
+  });
+
+  describe('part 2', () => {
+    test('test case', () => {
+      const input = dedent`
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+      `;
+      expect(solvePart2(input)).toBe(2);
+    });
+
+    test('real puzzle', () => {
+      const result = solvePart2(day9Input);
       expect(result).toBe(2954600);
     });
   });
--- days/day09/day9.ts
+++ days/day09/day9.ts
@@ -39,3 +39,32 @@ export function solvePart1 (input: string): number {
   const values = parseInput(input);
   return values.map(guessNextValue).reduce(sum);
 }
+
+const guessPreviousValue = (sequence: Array<number>): number => {
+  let hasFoundLimit = false;
+  let differences: Array<Array<number>> = [];
+  let sequenceToDiff = sequence;
+
+  while (!hasFoundLimit) {
+    const nextDifferences = sequenceToDiff.map((number, index) => {
+      if (index === 0) return undefined;
+      return number - sequenceToDiff[index - 1];
+    }).filter(isNumber);
+    differences.unshift(nextDifferences);
+    sequenceToDiff = nextDifferences;
+    if (nextDifferences.every(difference => difference === 0)) hasFoundLimit = true;
+  }
+
+  const leadingDifference = differences.reduce((previous, differenceRow, index) => {
+    if (index === 0) return previous;
+    const nextDifference = differenceRow[0] - previous;
+    return nextDifference;
+  }, 0);
+
+  return sequence[0] - leadingDifference;
+};
+
+export function solvePart2 (input: string): number {
+  const values = parseInput(input);
+  return values.map(guessPreviousValue).reduce(sum);
+}
